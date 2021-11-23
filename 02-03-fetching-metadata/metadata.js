f7c86566e71d5731c4a0561302929beb55b0dc79
Vladislav <ivanovlad2010@gmail.com>
2021-11-23 08:29:18 +0300

    fs: fetching metadata

---
+++ 02-03-fetching-metadata/metadata.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const file = process.argv[2];
+
+function printMetadata(file) {
+    try {
+        const fileStats = fs.statSync(file);
+        console.log(fileStats);
+    } catch (e) {
+        console.error("Error reading file path:", file);
+    }
+}
+
+printMetadata(file);
